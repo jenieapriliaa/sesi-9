ec484e59dcd400f059787168ec3f26b51009f45d
jenieapriliaa <127836861+jenieapriliaa@users.noreply.github.com>
2023-05-09 17:02:39 +0800

    Add files via upload

---
+++ Jeni/sesi9/javascript02.js
@@ -0,0 +1,2 @@
+document.getElementById("teks1").style.color = "#8FBC8F";
+document.getElementById("teks2").style.color = "#D8BFD8";
---
+++ Jeni/sesi9/javascript02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+    <div id="teks1">
+        <p>Hari Ini Belajar JavaScript</p>
+    </div>
+    <div id="teks2">
+        <p>Kabar Baik Bro</p>
+</div>   
+<script src="javascript02.js"></script>
+
+</body>
+</html>
